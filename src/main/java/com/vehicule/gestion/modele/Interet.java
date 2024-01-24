f6fb049eb3f6d3e36257a885760548202e377926
Fiderana-niavo <118703864+Fiderana-niavo@users.noreply.github.com>
2024-01-24 19:57:05 +0300

    goo

---
+++ src/main/java/com/vehicule/gestion/modele/Interet.java
@@ -0,0 +1,23 @@
+package com.vehicule.gestion.modele;
+
+public class Interet {
+    String idInteret;
+    float valeur;
+    
+
+    public float getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(float valeur) {
+        this.valeur = valeur;
+    }
+
+    public String getIdInteret() {
+        return idInteret;
+    }
+
+    public void setIdInteret(String idInteret) {
+        this.idInteret = idInteret;
+    }
+}

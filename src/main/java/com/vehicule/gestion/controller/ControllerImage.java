b3e1939aaafd764e3ecf3e6545b00edde907a71a
SantatraM <121082262+SantatraM@users.noreply.github.com>
2024-01-24 07:20:04 +0100

    commit

--- src/main/java/com/vehicule/gestion/controller/ControllerImage.java
+++ src/main/java/com/vehicule/gestion/controller/ControllerImage.java
@@ -1,13 +1,22 @@
 package com.vehicule.gestion.controller;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 import com.google.gson.Gson;
 import com.vehicule.gestion.modele.ApiResponse;
 import com.vehicule.gestion.modele.TraitementImage;
 
+import org.hibernate.cache.spi.FilterKey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ControllerImage {
@@ -17,7 +26,7 @@ public class ControllerImage {
     private ApiResponse reponse;
 
     // @Transactional(rollbackOn = Exception.class)
-    @GetMapping("/traitementimage}")
+    @GetMapping("/traitementimage")
     public ResponseEntity<String> traitementImage() {
         try {
             TraitementImage timage = new TraitementImage();
@@ -30,4 +39,33 @@ public class ControllerImage {
         }
     }
 
+    @GetMapping("/traitementimage/{liens}")
+    public ResponseEntity<String> existanceImage(@PathVariable("liens") String liens) {
+        try {
+            File file = new File(liens);
+            // timage.setCheminLocal("C:/Users/ITU/Desktop/VehiculeSpring/img/Peugeot-405-02.jpg");
+            return ResponseEntity.ok(String.valueOf(file.exists()));
+        } catch (Exception e) {
+            // e.printStackTrace();
+            reponse = new ApiResponse(e.getMessage(), null);
+            return ResponseEntity.status(500).body(gson.toJson(reponse));
+        }
+    }
+
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
+
+    @GetMapping("/uploadimage")
+    public String displayUploadForm() {
+        return "imageupload/index";
+    }
+
+    @PostMapping("/upload")
+    public String uploadImage(Model model, @RequestParam("image") MultipartFile file) {
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
+        return "imageupload/index";
+    }
 }
--- src/main/java/com/vehicule/gestion/modele/TraitementImage.java
+++ src/main/java/com/vehicule/gestion/modele/TraitementImage.java
@@ -1,6 +1,8 @@
 package com.vehicule.gestion.modele;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 // import java.io.IOException;
 // import java.net.http.HttpClient;
 // import java.nio.file.Files;
@@ -10,6 +12,7 @@ import java.util.Base64;
 
 import org.apache.commons.io.FileUtils;
 // import org.springframework.boot.autoconfigure.ssl.SslProperties.Bundles.Watch.File;
+import org.apache.tomcat.util.http.fileupload.FileItemStream;
 
 public class TraitementImage {
     String urlServeurStockage;
@@ -19,6 +22,7 @@ public class TraitementImage {
     }
 
     public TraitementImage(String urlServeurStockage) {
+        this.setUrlServeurStockage("https://api.imgbb.com/1/upload");
         this.urlServeurStockage = urlServeurStockage;
     }
 
@@ -33,6 +37,21 @@ public class TraitementImage {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
         File file = new File(this.getCheminLocal());
         FileUtils.writeByteArrayToFile(file, decodedBytes);
+    }
+
+    public void hebergementImage(FileItemStream item) throws Exception {
+        File file = new File(this.getCheminLocal());
+        // File savedFile = new
+        File savedFile = new File(file.getAbsolutePath() + File.separator + item.getName());
+        FileOutputStream fos = new FileOutputStream(savedFile);
+        InputStream is = item.openStream();
+        int x = 0;
+        byte[] b = new byte[1024];
+        while ((x = is.read(b)) != -1) {
+            fos.write(b, 0, x);
+        }
+        fos.flush();
+        fos.close();
     }
 
     public String getUrlServeurStockage() {

05b7476f4e3c9e93453c47f93a5668cc2e1980dd
SantatraM <121082262+SantatraM@users.noreply.github.com>
2024-01-26 11:58:19 +0100

    commit

--- src/main/java/com/vehicule/gestion/modele/Utilisateur.java
+++ src/main/java/com/vehicule/gestion/modele/Utilisateur.java
@@ -1,7 +1,4 @@
-// package com.vehicule.gestion.modele;
-
-
-
+package com.vehicule.gestion.modele;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -9,116 +6,39 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import com.vehicule.gestion.tools.Role;
 
-// import java.sql.Date;
-// import java.util.Collection;
-// import java.util.List;
-// import java.util.Optional;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-// import org.springframework.security.core.GrantedAuthority;
-// import org.springframework.security.core.authority.SimpleGrantedAuthority;
-// import org.springframework.security.core.userdetails.UserDetails;
-
-// import com.vehicule.gestion.tools.Role;
-
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.EnumType;
-// import jakarta.persistence.Enumerated;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-
-// @Entity
-// public class Utilisateur implements UserDetails{
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     @Column(name="idutilisateur")
-//     String idUtilisateur;
-//     @Column(name="nomutilisateur")
-//     String nomUtilisateur;
-//     String prenom;
-//     String adresse;
-//     String mail;
-//     @Column(name="motdepasse")
-//     String motDePasse;
-//     int sexe;
-//     @Column(name="datenaissance")
-//     Date dateNaissance;
-    
-//     @Enumerated(EnumType.STRING)
-//     Role role;
-//     // @JsonIgnore
-//     // @OneToMany(mappedBy = "utilisateur")
-//     // List<Annonce> annonce;
-
-//     // public List<Annonce> getAnnonce() {
-//     //     return annonce;
-//     // }
-
-//     // public void setAnnonce(List<Annonce> annonce) {
-//     //     this.annonce = annonce;
-//     // }
-
-    
-//   @Override
-//   public String toString() {
-//     return "Utilisateur [login=" + mail + ", motdepasse=" + motDePasse + "]";
-//   }
-
-//   @Override
-//   public Collection<? extends GrantedAuthority> getAuthorities() {
-//     return List.of(new SimpleGrantedAuthority(role.name())) ;
-//   }
-
-//   @Override
-//   public String getPassword() {
-//     return getMotDePasse();
-//   }
-
-//   @Override
-//   public String getUsername() {
-//     return getMail();
-//   }
-
-//   @Override
-//   public boolean isAccountNonExpired() {
-//     return true;
-//   }
-
-//   @Override
-//   public boolean isAccountNonLocked() {
-//     return true;
-//   }
-
-//   @Override
-//   public boolean isCredentialsNonExpired() {
-//     return true;
-//   }
-=======
 @Entity
-public class Utilisateur implements UserDetails{
+public class Utilisateur implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="idutilisateur")
-    String idUtilisateur;
-    @Column(name="nomutilisateur")
+    @Column(name = "idutilisateur")
+    String id;
+    @Column(name = "nomutilisateur")
     String nomUtilisateur;
     String prenom;
     String adresse;
     String mail;
-    @Column(name="motdepasse")
+    @Column(name = "motdepasse")
     String motDePasse;
     int sexe;
-    @Column(name="datenaissance")
+    @Column(name = "datenaissance")
     Date dateNaissance;
-    
+
     @Enumerated(EnumType.STRING)
     Role role;
-    // @JsonIgnore
-    // @OneToMany(mappedBy = "utilisateur")
-    // List<Annonce> annonce;
 
     public Utilisateur(String nomUtilisateur, String prenom, String adresse, String mail, String motDePasse, int sexe,
             Date dateNaissance, Role role) throws Exception {
@@ -135,143 +55,119 @@ public class Utilisateur implements UserDetails{
     public Utilisateur() {
     }
 
-    // public void setAnnonce(List<Annonce> annonce) {
-    //     this.annonce = annonce;
-    // }
+    public Utilisateur(String mail) {
+        this.mail = mail;
+    }
 
-    
-  @Override
-  public String toString() {
-    return "Utilisateur [login=" + mail + ", motdepasse=" + motDePasse + "]";
-  }
+    @Override
+    public String toString() {
+        return "Utilisateur [login=" + mail + ", motdepasse=" + motDePasse + "]";
+    }
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority(role.name())) ;
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
-  @Override
-  public String getPassword() {
-    return getMotDePasse();
-  }
+    @Override
+    public String getPassword() {
+        return getMotDePasse();
+    }
 
-  @Override
-  public String getUsername() {
-    return getMail();
-  }
+    @Override
+    public String getUsername() {
+        return getMail();
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
->>>>>>> Stashed changes
+    public Role getRole() {
+        return role;
+    }
 
-//   @Override
-//   public boolean isEnabled() {
-//     return true;
-//   }
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
+    public String getMotDePasse() {
+        return motDePasse;
+    }
 
-//     public Role getRole() {
-//         return role;
-//     }
-
-    public void setMotDePasse(String motDePasse) throws Exception{
-        if(motDePasse.equals("")|| motDePasse==null){
+    public void setMotDePasse(String motDePasse) throws Exception {
+        if (motDePasse.equals("") || motDePasse == null) {
             throw new Exception("Insertion mot de passe obligatoire");
         }
         this.motDePasse = motDePasse;
     }
 
-//     public String getMotDePasse() {
-//         return motDePasse;
-//     }
+    public String getAdresse() {
+        return adresse;
+    }
 
-//     public void setMotDePasse(String motDePasse) throws Exception{
-//         if(motDePasse.equals("")|| motDePasse==null){
-//             throw new Exception("Insertion mot de passe obligatoire");
-//         }
-//         this.motDePasse = motDePasse;
-//     }
+    public String getMail() {
+        return mail;
+    }
 
-    public void setMail(String mail) throws Exception{
-        if(mail.equals("")|| mail==null){
+    public void setMail(String mail) throws Exception {
+        if (mail.equals("") || mail == null) {
             throw new Exception("Insertion mail obligatoire");
         }
         this.mail = mail;
     }
 
-    public void setAdresse(String adresse)throws Exception {
-        if(adresse.equals("")|| adresse==null){
+    public void setAdresse(String adresse) throws Exception {
+        if (adresse.equals("") || adresse == null) {
             throw new Exception("Insertion adresse obligatoire");
         }
         this.adresse = adresse;
     }
 
-//     public void setMail(String mail) throws Exception{
-//         if(mail.equals("")|| mail==null){
-//             throw new Exception("Insertion mail obligatoire");
-//         }
-//         this.mail = mail;
-//     }
+    public String getNom() {
+        return nomUtilisateur;
+    }
 
-//     public void setAdresse(String adresse)throws Exception {
-//         if(adresse.equals("")|| adresse==null){
-//             throw new Exception("Insertion adresse obligatoire");
-//         }
-//         this.adresse = adresse;
-//     }
+    public String getPrenom() {
+        return prenom;
+    }
 
-    public void setPrenom(String prenom)throws Exception {
-        if(prenom.equals("")|| prenom==null){
+    public void setPrenom(String prenom) throws Exception {
+        if (prenom.equals("") || prenom == null) {
             throw new Exception("Insertion prenom obligatoire");
         }
         this.prenom = prenom;
     }
 
-    public void setNom(String nom) throws Exception{
-        if(nom.equals("")|| nom==null){
+    public void setNom(String nom) throws Exception {
+        if (nom.equals("") || nom == null) {
             throw new Exception("Insertion nom obligatoire");
         }
         this.nomUtilisateur = nom;
     }
 
-    public boolean isNomDuplacated(Optional<Utilisateur> modele){
-        if(modele.isEmpty()){
-             return true;
+    public boolean isNomDuplacated(Optional<Utilisateur> modele) {
+        if (modele.isEmpty()) {
+            return true;
         }
         return false;
-    }   
-
-    public void setNom(String nom) throws Exception{
-        if(nom.equals("")|| nom==null){
-            throw new Exception("Insertion nom obligatoire");
-        }
-        this.nomUtilisateur = nom;
     }
-
-    public boolean isNomDuplacated(Optional<Utilisateur> modele){
-        if(modele.isEmpty()){
-             return true;
-        }
-        return false;
-    }   
 
     public int getSexe() {
         return sexe;
@@ -285,7 +181,6 @@ public class Utilisateur implements UserDetails{
         this.setSexe(Integer.valueOf(sexe));
     }
 
-
     public Date getDateNaissance() {
         return dateNaissance;
     }
@@ -294,16 +189,25 @@ public class Utilisateur implements UserDetails{
         this.dateNaissance = dateNaissance;
     }
 
-    public void setDateNaissance(String dateNaissance) {
-        this.setDateNaissance(Date.valueOf(dateNaissance));
-    }
-
-}
-    public void setDateNaissance(String dateNaissance) {
-        this.setDateNaissance(Date.valueOf(dateNaissance));
     public void setDateNaissance(String dateNaissance) throws Exception {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         this.setDateNaissance(dateFormat.parse(dateNaissance));
+    }
+
+    public String getIdUtilisateur() {
+        return id;
+    }
+
+    public void setIdUtilisateur(String id) {
+        this.id = id;
+    }
+
+    public String getNomUtilisateur() {
+        return nomUtilisateur;
+    }
+
+    public void setNomUtilisateur(String nomUtilisateur) {
+        this.nomUtilisateur = nomUtilisateur;
     }
 
 }

78b08aaf90bf6ea962d48162dc1c6834ce2f0a9f
SantatraM <121082262+SantatraM@users.noreply.github.com>
2024-01-29 14:18:26 +0100

    commit

--- src/main/java/com/vehicule/gestion/controller/ControllerInteret.java
+++ src/main/java/com/vehicule/gestion/controller/ControllerInteret.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,14 +21,11 @@ import jakarta.transaction.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class ControllerInteret {
-    
-    
-    // @Autowired
-    // private InteretService entiteService;
-    // private Gson gson = new Gson();
-   // private ApiResponse response;
+    @Autowired
+    private InteretService entiteService;
+    private Gson gson = new Gson();
+    private ApiResponse response;
 
     // @GetMapping("/interets")
     // public ResponseEntity<String> getAll() {
@@ -39,11 +35,11 @@ public class ControllerInteret {
     // return ResponseEntity.ok(gson.toJson(response));
     // } catch (Exception e) {
     // response = new ApiResponse(e.getMessage(), null);
-    // return ResponseEntity.status(500).body(gson.toJson(response));
+    // return ResponseEntity.status(500).body(e.getMessage());
     // }
     // }
 
-    // @PostMapping("/interet")
+    // @GetMapping("/interet/")
     // public ResponseEntity<String> save(@RequestBody Interet interet) {
     // try {
     // response = new ApiResponse("", entiteService.save(interet));
@@ -57,14 +53,14 @@ public class ControllerInteret {
     // @Transactional(rollbackOn = Exception.class)
     // @PostMapping("/interet/{id}")
     // public ResponseEntity<String> deleteById(@PathVariable("id") String id) {
-    //     try {
-    //         entiteService.deleteById(id);
-    //         return ResponseEntity.ok("Interet id = " + id + " deleted successfully.");
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //         response = new ApiResponse(e.getMessage(), null);
-    //         return ResponseEntity.status(500).body(gson.toJson(response));
-    //     }
+    // try {
+    // entiteService.deleteById(id);
+    // return ResponseEntity.ok("Interet id = " + id + " deleted successfully.");
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // response = new ApiResponse(e.getMessage(), null);
+    // return ResponseEntity.status(500).body(gson.toJson(response));
+    // }
     // }
 
 }

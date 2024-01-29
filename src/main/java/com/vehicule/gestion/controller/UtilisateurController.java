22bb6b4c1bdfcbfcc18c15175413ce1eee0a345d
Jeremie04 <118689934+Jeremie04@users.noreply.github.com>
2024-01-29 22:50:16 +0100

    another try

--- src/main/java/com/vehicule/gestion/controller/UtilisateurController.java
+++ src/main/java/com/vehicule/gestion/controller/UtilisateurController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "/*")
 @RequestMapping("/initial")
 public class UtilisateurController {
 
